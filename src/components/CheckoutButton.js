c7166d01ca439478e070efdfe2c3e53740d97f41
Akashdeep <akash.kashhyap@gmail.com>
2023-03-17 17:39:37 +0530

    update checkout call

--- src/components/CheckoutButton.js
+++ src/components/CheckoutButton.js
@@ -1,22 +1,32 @@
-import React, { useState } from 'react'
-import jwtDecode from 'jwt-decode'
-const CheckoutButton= () => {
-    const session = useState( () => {
-        const jwtSession = window.localStorage.getItem('woo-session');
-        if( !jwtSession ) return null;
-        try {
-            const decoded = jwtDecode<{ data: { customer_id: string } }>(jwtSession);
-            return decoded.data.customer_id;
-        } catch( err ) {
-            console.warn("err:", err);
-            return null;
-        }
-    } )
-    const checkoutLink = () => {
-        window.open(`https://woocommerce-186938-3327038.cloudwaysapps.com/checkout?session_id=${session[0]}`)
+import React, { useState, useEffect } from 'react';
+import jwtDecode from 'jwt-decode';
+
+const CheckoutButton = () => {
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    const jwtSession = window.localStorage.getItem('woo-session');
+    if (jwtSession) {
+      try {
+        const decoded = jwtDecode<{ data: { customer_id: string } }>(jwtSession);
+        setSession(decoded.data.customer_id);
+      } catch (err) {
+        console.warn('Error decoding session:', err);
+      }
     }
-    return(
-        <button className='p-2 bg-slate-900 rounded-md text-white' onClick={ () => checkoutLink() }>Checkout</button>
-    )
-}
+  }, []);
+
+  const checkoutLink = () => {
+    if (session) {
+      window.open(`https://woocommerce-186938-3327038.cloudwaysapps.com/checkout?session_id=${session}`);
+    } else {
+      console.warn('Session not found');
+    }
+  };
+
+  return (
+    <button className='p-2 bg-slate-900 rounded-md text-white' onClick={checkoutLink}>Checkout</button>
+  );
+};
+
 export default CheckoutButton;
