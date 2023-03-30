6ffc09bbdd8184466ae7100cde1e60d57a310e3f
Akashdeep <akash.kashhyap@gmail.com>
2023-03-30 11:11:34 +0530

    ui

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -97,7 +97,7 @@ const Navigation = ({ siteLogoUrl }) => {
           )}
         </div>
         <div id="navbar-collapse-with-animation" className="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow sm:block">
-          <div className="flex flex-col gap-y-4 gap-x-0 mt-5 sm:flex-row sm:items-center sm:justify-end sm:gap-y-0 sm:gap-x-7 sm:mt-0 sm:pl-7">
+          <div className="flex flex-col gap-y-4 gap-x-0 mt-5 sm:flex-row sm:items-center sm:justify-end sm:gap-y-0 sm:gap-x-7 sm:mt-0 sm:pl-7 pr-4">
             {isAuthenticated ? (
               <>
                 <Link href="/account" legacyBehavior>
@@ -130,7 +130,7 @@ const Navigation = ({ siteLogoUrl }) => {
                   />
                 </svg>
                 {productsCount ? (
-                  <span className="absolute -top-2 -right-3">
+                  <span className="absolute -top-1 -right-3">
                     ({productsCount})
                   </span>
                 ) : (
